920c646fd36b390346771ee6598bd44ae4476669
Dav <davidpichard77@gmail.com>
2024-01-13 20:58:07 +0100

    add route to add event

--- src/app/api/event/route.ts
+++ src/app/api/event/route.ts
@@ -36,3 +36,46 @@ export async function PUT(request: Request) {
   );
   return Response.json({ result });
 }
+
+export async function POST(request: Request) {
+  const res = await request.json();
+  const sensor = res.sensor;
+  const oneSensor = await db.sensor.findUnique({
+    where: {
+      id: sensor,
+    },
+  });
+  if (!oneSensor) return;
+
+  const sensorUpdated = await db.sensor.update({
+    where: {
+      id: oneSensor.id,
+    },
+    data: {
+      events: {
+        create: [{ event: { create: { is_over: false } } }],
+      },
+    },
+    include: {
+      events: {
+        include: {
+          event: {
+            include: {
+              sensors: {
+                include: {
+                  sensor: true,
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  });
+  const latestEvent =
+    sensorUpdated.events[sensorUpdated.events.length - 1].event;
+
+  const { id, is_over, created_at, sensors } = latestEvent;
+
+  return Response.json({ id, is_over, created_at, sensors });
+}
